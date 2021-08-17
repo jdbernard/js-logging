f75edbc22a24f1f6b3557128654edce98fdee223
Jonathan Bernard <jonathan@jdbernard.com>
2021-08-16 23:03:57 -0500

    Fix NPM packaging.

--- dist/console-log-appender.d.ts
+++
@@ -1,8 +0,0 @@
-import { LogMessage, LogLevel } from './log-message';
-import LogAppender from './log-appender';
-export declare class ConsoleLogAppender implements LogAppender {
-    threshold: LogLevel;
-    constructor(threshold?: LogLevel);
-    appendMessage(msg: LogMessage): void;
-}
-export default ConsoleLogAppender;
