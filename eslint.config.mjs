5e167b7fb446683f013f105d9044fe001e4d6bd7
Jonathan Bernard <jonathan@jdbernard.com>
2026-05-07 07:28:48 -0500

    Include test in ESLint coverage.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tsParser from '@typescript-eslint/parser'
 import eslintJs from '@eslint/js'
 import eslintTs from 'typescript-eslint'
 
-const tsFiles = ['src/**/*.ts']
+const tsFiles = ['src/**/*.ts', 'test/**/*.ts']
 
 const customTypescriptConfig = {
   files: tsFiles,
